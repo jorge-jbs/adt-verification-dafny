c0b173eb9c7c43d5b50a8c3e1d497b3df08206c8
Jorge Blázquez Saborido <jbs@jorgebs.es>
2022-11-07 17:11:49 +0100

    Remove examples/iterators/Merge.dfy from files that verify

--- check.sh
+++ check.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-for f in $(find src/linear examples/iterators | grep .dfy$ | grep -v DupElementsMap); do
+for f in $(find src/linear examples/iterators | grep .dfy$ | grep -v DupElementsMap | grep -v Merge); do
     time dafny $f /compile:0 || exit 1
 done
