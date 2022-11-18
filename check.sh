75e5b67b0c96acc2f7b0a30c0ea46c271f1d2d22
Jorge Blázquez Saborido <jbs@jorgebs.es>
2022-11-18 13:24:40 +0100

    Remove DupElementsMap from not verified files

--- check.sh
+++ check.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-for f in $(find src/linear examples/iterators examples/*.dfy | grep .dfy$ | grep -v DupElementsMap | grep -v Merge | grep -v Quicksort | grep -v FilterEven); do
+for f in $(find src/linear examples/iterators examples/*.dfy | grep .dfy$ | grep -v Merge | grep -v Quicksort | grep -v FilterEven); do
     time dafny $f /compile:0 || exit 1
 done
