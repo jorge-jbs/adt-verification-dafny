21d4fa7995dd69253b7a16692b4d3affcdd75200
Jorge Blázquez Saborido <jbs@jorgebs.es>
2023-01-09 14:59:28 +0100

    Remove DupElementsMap from check.sh

--- check.sh
+++ check.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-for f in $(find src/linear examples/iterators | grep .dfy$ | grep -v Merge | grep -v Quicksort | grep -v FilterEven | grep -v FibPrefix); do
+for f in $(find src/linear examples/iterators | grep .dfy$ | grep -v Merge | grep -v Quicksort | grep -v FilterEven | grep -v FibPrefix | grep -v DupElementsMap); do
     time dafny $f /compile:0 || exit 1
 done
