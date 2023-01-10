5da1d943362e0ba285a76f0a96c7daebb180f1fe
Jorge Blázquez Saborido <jbs@jorgebs.es>
2023-01-10 12:50:38 +0100

    Remove CircularDoubleLinkedList from check.sh

--- check.sh
+++ check.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-for f in $(find src/linear examples/iterators | grep .dfy$ | grep -v ElimDupMap | grep -v FibPrefix); do
+for f in $(find src/linear examples/iterators | grep .dfy$ | grep -v ElimDupMap | grep -v FibPrefix | grep -v CircularDoublyLinkedList); do
     time dafny verify $f || exit 1
 done
