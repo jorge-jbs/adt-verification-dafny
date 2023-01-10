568abb33c711c1d90206dd5472c9d3308354b7c2
Jorge Blázquez Saborido <jbs@jorgebs.es>
2023-01-10 12:47:56 +0100

    Use `dafny verify` in check.sh

--- check.sh
+++ check.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 for f in $(find src/linear examples/iterators | grep .dfy$ | grep -v ElimDupMap | grep -v FibPrefix); do
-    time dafny $f /compile:0 || exit 1
+    time dafny verify $f || exit 1
 done
